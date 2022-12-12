e426b54e29fb1b72e28b0e324a7d487864c1df90
Yulia Naumenko <naumenkoyuliya@ukr.net>
2022-12-12 12:55:15 +0900

    added rspec for model

---
+++ spec/models/task_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe Task, type: :model do
+  let(:task) { Task.create!(title: 'Title', details: 'To many words in this details') }
+
+  describe '#initialize' do
+    context 'when correct param are given' do
+      it "creates a Task instance" do
+        expect(task.title).to eq('Title')
+      end
+    end
+
+    context 'when it is not given a title' do
+      it "Doesn't create a Task instance" do
+        task = Task.new(details: 'To many wordsin this details')
+        expect(task.errors.messages).to eq({:title=>["can't be blank"]})
+      end
+    end
+  end
+
+  describe '#truncate_details' do
+    it "should be 15 characters" do
+      expect(task.truncate_details).to eq("To many words i")
+    end
+  end
+end
